2831d45b4682d3241db662e8bc80429001e30b4b
Aditya Upadhyay <aditya.sidd.upadhyay@gmail.com>
2019-08-31 12:51:00 +0530

    typing error correction'

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn panic(_info: &PanicInfo) -> ! {
  */
 
 #[no_mangle]
-pub extern "C" fn _start() -> {
+pub extern "C" fn _start() -> ! {
     println!("Hellow World {}", "@");
     
     loop {}
